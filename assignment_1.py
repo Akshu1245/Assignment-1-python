f7946ad46c2892f1e65678d5762cee2c64963ca5
K S AKSHAY <160865551+Akshu1245@users.noreply.github.com>
2025-12-04 10:30:33 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,19 @@
+"""
+print(" The input for the numbers")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+"""
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+#Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+#Displaying results
+print("The sum is", addition)
+print("The subtraction is", subtraction)
+print("The multiplication is", multiplication)
+print("The division is", division)
---
+++ greeting.py
@@ -0,0 +1,11 @@
+# Ask the user for their first name
+first_name = input("Enter your first name: ")
+
+# Ask the user for their last name
+last_name = input("Enter your last name: ")
+
+# Combine the names
+full_name = first_name + " " + last_name
+
+# Print the greeting
+print(f"\nHello, {full_name}! Welcome to the Python program.")
